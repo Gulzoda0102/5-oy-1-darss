a3f8fea8f7fe5e27ec3059ed1087fa6f1c93fd1a
Gulzoda0102 <zodait2024@gmail.com>
2024-12-19 22:33:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+let name = prompt("Ismingizni kiriting:");
+let surname = prompt("Familiyangizni kiriting:");
+let age = prompt("Yoshingizni kiriting:");
+let subject = prompt("Yoqtirgan faningizni yozing:");
+let district = prompt("Yashaydigan tumaningizni kiriting:");
+
+
+console.log("Ism: " + name);
+console.log("Familiya: " + surname);
+console.log("Yosh: " + age); 
+console.log("Yoqtirgan fan: " + subject);
+console.log("Yashash manzili: " + district);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
